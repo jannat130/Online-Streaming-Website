20a710344c4ea3632442de3ed78dc1efa55575a1
Jannat <97015416+jannat130@users.noreply.github.com>
2023-04-08 00:15:50 +0600

    first commit

---
+++ Model/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+      </div>
+      <strong>&copy; 2022 </strong>
+      </div>
+    <!-- /.container -->
+</footer>
---
+++ View/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+     
+    </div>
+    <strong>&copy;2022 </strong>
+</footer>
